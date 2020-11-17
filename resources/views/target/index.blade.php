0f042209006e5da5886b2463162382406f328330
Takuya <takuyaouchi18@gmail.com>
2020-11-17 22:38:15 +0700

    delete sentence

--- resources/views/target/index.blade.php
+++ resources/views/target/index.blade.php
@@ -16,11 +16,11 @@
           <form method="GET" action="{{ route('target.create') }}">
             <button type="submit" class="btn btn-dark">
               新規登録
-            </button>
+            </button> 
           </form>
 
           <form method="GET" action="{{ route('target.index') }}" class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" name="serch" type="search" placeholder="検索" aria-label="Search">
+            <input class="form-control mr-sm-2" name="serch" type="search"　aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索する</button>
           </form>
           <form method="GET" action="{{ route('target.index') }}" class="form-inline my-2 my-lg-0">
